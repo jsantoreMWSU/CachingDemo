72f0f3eca5c1d22adef3b05f6167c4e4bde5c450
jsantore <jsantore@bridgew.edu>
2024-03-15 11:59:58 -0400

    initial recursion before caching

---
+++ RecursionWithGo.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("calculate which fibonacci number:")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("error reading - giving up ", err)
+	}
+	text = strings.TrimSpace(text)
+	num, err := strconv.Atoi(text)
+	if err != nil {
+		log.Fatal("you didn't put in an int - giving up ", err)
+	}
+	fmt.Println("Fib of ", num, " is ", fib(num))
+	//fmt.Println("Factorial of ", num, " is ", fact(num))
+}
+
+func fib(num int) int {
+	if num <= 2 {
+		return 1
+	}
+	return fib(num-1) + fib(num-2)
+}
+
+func fact(num int) int64 {
+	if num <= 1 {
+		return 1
+	}
+	return int64(num) * fact(num-1)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module CachingDemo
+
+go 1.21
